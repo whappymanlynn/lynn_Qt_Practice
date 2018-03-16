8ee47d546e37b39d9a34c3a25e24b696c2c836bc
whappymanlynn <whappymanlynn@outlook.com>
2018-03-17 00:00:28 +0800

    learn the demo from Qt Gui 4

--- FindDialog/widget.cpp
+++ FindDialog/widget.cpp
@@ -3,9 +3,78 @@
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
+    QVBoxLayout *leftlayout = new QVBoxLayout;
+
+    QHBoxLayout * left_top_layout = new QHBoxLayout;
+    tipLabel = new QLabel(tr("&Find what:"));
+    keywordLineEdit = new QLineEdit;
+    tipLabel->setBuddy(keywordLineEdit);
+
+    matchCheckBox =new QCheckBox(tr("&Match case"));
+    backCheckBox = new QCheckBox(tr("search backword"));
+
+
+    left_top_layout->addWidget(tipLabel);
+    left_top_layout->addWidget(keywordLineEdit);
+
+    leftlayout->addLayout(left_top_layout);
+    leftlayout->addWidget(matchCheckBox);
+    leftlayout->addWidget(backCheckBox);
+
+
+    QVBoxLayout * rightlayout = new QVBoxLayout;
+
+
+    findButton = new QPushButton(tr("&find"));
+    findButton->setDefault(true);
+    findButton->setEnabled(false);
+
+    closeButton = new QPushButton(tr("&Close"));
+    rightlayout->addWidget(findButton);
+    rightlayout->addWidget(closeButton);
+    rightlayout->addStretch();
+
+
+    QHBoxLayout * mainlayout = new QHBoxLayout;
+    mainlayout->addLayout(leftlayout);
+    mainlayout->addLayout(rightlayout);
+    setLayout(mainlayout);
+
+    setWindowTitle(tr("find file"));
+    setFixedHeight(sizeHint().height());
+    connect(keywordLineEdit,SIGNAL(textChanged(const QString&)),
+            this,SLOT(enabledFindButton(const QString &)));
+    connect(findButton,SIGNAL(clicked()),
+            this,SLOT(findClicked()));
+    connect(closeButton,SIGNAL(clicked()),
+            this,SLOT(close()));
+
+
+
+
+
 }
 
 Widget::~Widget()
 {
 
+}
+void Widget::findClicked()
+{
+    QString text = keywordLineEdit->text();
+    Qt::CaseSensitivity cs = matchCheckBox->isCheckable()?Qt::CaseSensitive:
+                                                           Qt::CaseInsensitive;
+    if(backCheckBox->isCheckable()){
+        emit findPrevious(text,cs);
+    }
+    else
+    {
+        emit findNext(text,cs);
+    }
+
+}
+
+void Widget::enabledFindButton(const QString &text)
+{
+    findButton->setEnabled(!text.isEmpty());
 }
--- FindDialog/widget.h
+++ FindDialog/widget.h
@@ -2,14 +2,38 @@
 #define WIDGET_H
 
 #include <QWidget>
-
+#include<QPushButton>
+#include<QCheckBox>
+#include<QLabel>
+#include<QLineEdit>
+#include<QGridLayout>
+#include<QHBoxLayout>
+#include<QVBoxLayout>
 class Widget : public QWidget
 {
     Q_OBJECT
 
 public:
     Widget(QWidget *parent = 0);
+
     ~Widget();
+signals:
+    void  findNext(const QString &str , Qt::CaseSensitivity cs);
+    void  findPrevious(const QString &str,Qt::CaseSensitivity cs);
+
+ private slots:
+    void findClicked();
+    void enabledFindButton(const QString &text);
+
+
+
+ private:
+    QLabel *tipLabel;
+    QLineEdit * keywordLineEdit;
+    QCheckBox  * matchCheckBox;
+    QCheckBox  * backCheckBox;
+    QPushButton * findButton;
+    QPushButton * closeButton;
 };
 
 #endif // WIDGET_H
